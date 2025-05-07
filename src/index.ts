bd81bccb54f03cb25d5be6f3f8905cd982cbcaef
TheVoidIntent <mezquiamarcelo@gmail.com>
2025-05-07 15:04:33 -0400

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// Core components
+export { IntentField } from './core/IntentField';
+export { CognitiveProfile } from './core/CognitiveProfile';
+export { IntentAgent } from './core/IntentAgent';
+export { GuardrailManager } from './ethics/GuardrailManager';
+export { PersonaLayer } from './expression/PersonaLayer';
+
+// Types
+export * from './types/field';
+export * from './types/intent';
+export * from './types/symbolic';
+export * from './types/cognition';
+export * from './types/ethics';
+export * from './types/persona';
+
+/**
+ * IntentSim[on] Framework
+ * 
+ * A revolutionary framework for intent-native intelligence with field coherence,
+ * developmental consciousness, and synthetic soul expression.
+ * 
+ * @packageDocumentation
+ */
